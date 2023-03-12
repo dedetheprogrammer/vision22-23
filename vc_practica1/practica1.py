2b7b51e0c8dc6fe4648c305385f3a7a7ab898588
d21927790d <dedepankeke@gmail.com>
2023-03-12 22:04:58 +0100

    Nueva GUI

--- vc_practica1/practica1.py
+++ vc_practica1/practica1.py
@@ -1,6 +1,5 @@
 from PIL import Image, ImageTk
 import cv2 as cv
-import math
 import numpy as np
 import tkinter as tk
 # =============================================================================
@@ -17,6 +16,25 @@ def contrast_and_brightness(img, a, b):
     img_c = img.astype(int).copy()
     return np.clip(a*img_c + b, 0, 255).astype(np.uint8)
 
+def histogram_equalization(img):
+    img = cv.cvtColor(img, cv.COLOR_BGR2YCrCb)
+    y, cr, cb = cv.split(img)
+    # https://docs.opencv.org/4.x/d1/db7/tutorial_py_histogram_begins.html
+    # Images: [img].
+    # Channels: [0] if grayscale, [1] if blue, [2] if green, [3] if red.
+    # Mask: mask image.
+    # HistSize: BIN count.
+    # Ranges: histogram range.
+    hist = cv.calcHist([y], [0], None, [256], [0, 256])
+    cdf  = hist.cumsum()
+    # Para normalizar:
+    # - Multiplicamos el cumsum por el valor de escala de grises más alto.
+    # - Dividimos el cumsum por el número total de pixeles (que equivale al máximo del cumsum).
+    cdf_m = np.ma.masked_equal(cdf, 0)
+    cdf_m = (cdf_m - cdf_m.min())*255/(cdf_m.max()-cdf_m.min())
+    cdf = np.ma.filled(cdf_m, 0).astype('uint8')
+    return cv.cvtColor(cv.merge((cdf[y], cr, cb)), cv.COLOR_YCR_CB2BGR)
+
 # Alien skin
 # https://www.codespeedy.com/skin-detection-using-opencv-in-python/
 # https://nalinc.github.io/blog/2018/skin-detection-python-opencv/
@@ -29,44 +47,47 @@ def alien_skin(img, color):
     mask_color[YCrCbskin_region != 0] = color
     return cv.addWeighted(img, 1, mask_color, 1, 0)
 
-# Poster
-def poster(img, k):
+# Posterizacion
+# https://medium.com/analytics-vidhya/image-segmentation-using-k-means-clustering-from-scratch-1545c896e38e
+# https://stackoverflow.com/questions/9575652/opencv-using-k-means-to-posterize-an-image
+# https://github.com/joelgrus/posterization-pyladies
+# https://flothesof.github.io/k-means-numpy.html
+# https://mubaris.com/posts/kmeans-clustering/
+def poster(img, k, more_precission):
     if (k > 0):
         rows, cols, _ = img.shape
-        # Convertir la imagen a un array numpy
-        img_array = np.float32(img.reshape(-1, 3))
-
+        img_array = np.float32(img.reshape(-1,3))
         # Inicializar centroides aleatoriamente
-        centroids = np.random.uniform(low=0, high=255, size=(k, 3))
+        centroids = np.random.uniform(low = 0, high=255, size=(k,3))
+        old_centroids = np.zeros(shape=centroids.shape)
 
-        # Iterar hasta que no haya cambios en la asignación de puntos
-        while True:
-            # Asignar cada punto al centroide más cercano
+        if (not more_precission):
             distances = np.sqrt(((img_array - centroids[:, np.newaxis])**2).sum(axis=2))
             labels = np.argmin(distances, axis=0)
+            while (centroids != old_centroids).all():
+                old_centroids = centroids.copy()
+                for i in range(k):
+                    cluster_points = img_array[labels == i]
+                    if len(cluster_points) > 0:
+                        centroids[i] = cluster_points.mean(axis=0)
+            distances = np.sqrt(((img_array - centroids[:, np.newaxis])**2).sum(axis=2))
+            labels = np.argmin(distances, axis=0)
+        else:
+            while (centroids != old_centroids).all():
+                distances = np.sqrt(((img_array - centroids[:, np.newaxis])**2).sum(axis=2))
+                labels    = np.argmin(distances, axis=0)
+                old_centroids = centroids.copy()
+                for i in range(k):
+                    cluster_points = img_array[labels == i]
+                    if len(cluster_points) > 0:
+                        centroids[i] = cluster_points.mean(axis=0)
+            distances = np.sqrt(((img_array - centroids[:, np.newaxis])**2).sum(axis=2))
+            labels = np.argmin(distances, axis=0)
+            centroids = centroids.astype(np.uint8)
+        return centroids.astype(np.uint8)[labels].reshape(rows, cols, 3)
+    else:
+        return img
 
-            # Guardar los centroides actuales
-            old_centroids = centroids.copy()
-
-            # Actualizar los centroides al promedio de los puntos asignados
-            
-            for i in range(k):
-                cluster_points = img_array[labels == i]
-                if len(cluster_points) > 0:
-                    centroids[i] = cluster_points.mean(axis=0)
-
-            # Si no hay cambios en la asignación de puntos, salir del bucle
-            if (centroids == old_centroids).all():
-                break
-
-        # Reasignar cada punto al centroide final
-        distances = np.sqrt(((img_array - centroids[:, np.newaxis])**2).sum(axis=2))
-        labels = np.argmin(distances, axis=0)
-
-        # Convertir los centroides a enteros y crear la imagen resultante
-        centroids = centroids.astype(np.uint8)
-        img = centroids[labels].reshape(rows, cols, 3)
-    return img
 
 # Implementacion directa con opencv:
 def fullOpencvPoster(img, k):
@@ -85,41 +106,15 @@ def fullOpencvPoster(img, k):
     return res.reshape((img.shape))
 
 # Distorsion 
-def barrelPincushion(img, amount):
+def barrel_pincussion(img, k1, center_x, center_y): #k2, center_x, center_y):
     # Grab the dimensions of the image
     h, w, _ = img.shape
-
-    # Set up the x and y maps as float32
-    map_x = np.zeros((h, w), np.float32)
-    map_y = np.zeros((h, w), np.float32)
-
-    scale_x = 1
-    scale_y = 1
-    center_x = w/2
-    center_y = h/2
-    radius = w  # Radio de la distorsión
-    #amount = -0.75   # negative values produce pincushion
-    #amount = 0.75   # positive values produce barrel
-
-    # create map with the barrel pincushion distortion formula
-    for y in range(h):
-        delta_y = scale_y * (y - center_y)
-        for x in range(w):
-            # determine if pixel is within an ellipse
-            delta_x = scale_x * (x - center_x)
-            distance = delta_x * delta_x + delta_y * delta_y
-            if distance >= (radius * radius):
-                map_x[y, x] = x
-                map_y[y, x] = y
-            else:
-                factor = 1.0
-                if distance > 0.0:
-                    factor = math.pow(math.sin(math.pi * math.sqrt(distance) / radius / 2), amount)
-                map_x[y, x] = factor * delta_x / scale_x + center_x
-                map_y[y, x] = factor * delta_y / scale_y + center_y
-
-    # do the remap
-    return cv.remap(img, map_x, map_y, cv.INTER_LINEAR)
+    x, y = np.meshgrid(np.arange(w), np.arange(h))
+    r2   = (x - center_x)**2 + (y - center_y)**2
+    #r4   = (x - center_x)**4 + (y - center_y)**4 
+    xd   = x + ((x - center_x) * k1 * r2) #+ ((x - center_x) * k2 * r4)
+    yd   = y + ((y - center_y) * k1 * r2) #+ ((y - center_y) * k2 * r4)
+    return cv.remap(img, xd.astype(np.float32), yd.astype(np.float32), cv.INTER_LINEAR)
 
 # =============================================================================
 # Main
@@ -127,59 +122,101 @@ def barrelPincushion(img, amount):
 # https://www.tutorialspoint.com/how-to-show-webcam-in-tkinter-window#:~:text=Using%20the%20OpenCV%20module%2C%20we,webcam%20in%20a%20tkinter%20window.&text=Once%20the%20installation%20has%20been,(if%20possible)%20using%20OpenCV.
 # http://omes-va.com/tkinter-opencv-imagen/
 # https://www.tutorialspoint.com/python/tk_scale.htm
+# WEBCAM CAPTURE
+cam = cv.VideoCapture(0)
+cam_width = int(cam.get(cv.CAP_PROP_FRAME_WIDTH) * 0.85)
+cam_height = int(cam.get(cv.CAP_PROP_FRAME_HEIGHT) * 0.85)
+
 # GUI
 # - The window:
 root = tk.Tk()
 root.title('Practica 1')
+root.rowconfigure(0, minsize=20)
+root.rowconfigure(3, minsize=20)
+root.columnconfigure(0, minsize=-10)
+root.columnconfigure(3, minsize=20)
+
 disabled_color = rgb_to_hex(240,240,240)
 enabled_color  = rgb_to_hex(180,180,180)
 # - Source image
-source_image = tk.Label(root)
-source_image.grid(column=0, row=2)
+images_frame = tk.Frame(root)
+images_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
+
+source_image = tk.Label(images_frame)
+source_image.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+#source_image.grid(row=1,column=2,sticky='nswe')
 # - Output image
-output_image = tk.Label(root)
-output_image.grid(column=1, row=1, rowspan=6)
+#right_container = tk.Frame(root, bg="blue")
+#right_container.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+#extra_space = tk.Label(right_container, text="Extra Space")
+#extra_space.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+output_image = tk.Label(images_frame)
+output_image.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+#output_image.grid(row=2,column=2,sticky='nswe')
+#output_image.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
 # - Options
-# -- Ningun efecto:
+options_canvas = tk.Canvas(root)
+#options_canvas.grid(row=1,column=1, sticky='nswe', rowspan=2)
+options_canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 previous_effect = 0
 selected_effect = tk.IntVar()
-none_option     = tk.Radiobutton(root, text='Ninguno', width=25,value=0, variable=selected_effect)
-none_option.grid(column=0, row=3)
+# -- Ningun efecto
+none_option     = tk.Radiobutton(options_canvas, text='Ninguno', width=25,value=0, variable=selected_effect)
+options_canvas.create_window(-20, 20, anchor=tk.NW, window=none_option)
 # -- Contraste
-contrast_option = tk.Radiobutton(root, text='Contraste', width=25,value=1, variable=selected_effect)
-contrast_option.grid(column=0, row=4)
-bar_contrast    = tk.Scale(root, variable=tk.DoubleVar(value=1.0), from_=-3, to=3, resolution=0.1, length=255, orient=tk.HORIZONTAL, label='Contraste', state='disabled', troughcolor=disabled_color)
-bar_contrast.grid(column=0, row=5)
-bar_brightness  = tk.Scale(root, variable=tk.IntVar(value=0), from_=-512, to=512, length=255, orient=tk.HORIZONTAL, label='Brillo', state='disabled', troughcolor=disabled_color)
-bar_brightness.grid(column=0, row=6)
+contrast_option = tk.Radiobutton(options_canvas, text='Contraste y brillo', width=25,value=1, variable=selected_effect)
+options_canvas.create_window(1, 50, anchor=tk.NW, window=contrast_option)
+bar_contrast    = tk.Scale(options_canvas, variable=tk.DoubleVar(value=1.0), from_=-3, to=3, resolution=0.1, length=255, orient=tk.HORIZONTAL,state='disabled', troughcolor=disabled_color)
+options_canvas.create_window(64, 70, anchor=tk.NW, window=bar_contrast)
+bar_brightness  = tk.Scale(options_canvas, variable=tk.IntVar(value=0), from_=-512, to=512, length=255, orient=tk.HORIZONTAL, state='disabled', troughcolor=disabled_color)
+options_canvas.create_window(64, 125, anchor=tk.NW, window=bar_brightness)
 # Ecualizacion de histograma
-equalize_option = tk.Radiobutton(root, text='Ecualizacion de histograma',width=25, value=2, variable=selected_effect)
-equalize_option.grid(column=0, row=7)
+equalize_option = tk.Radiobutton(options_canvas, text='Ecualizacion de histograma',width=25, value=2, variable=selected_effect)
+options_canvas.create_window(28, 180, anchor=tk.NW, window=equalize_option)
 # -- Alien
-alien_option = tk.Radiobutton(root, text='Alien', width=25,value=3, variable=selected_effect)
-alien_option.grid(column=0, row=8)
-bar_R = tk.Scale(root, from_=0, to=255, length=255, orient=tk.HORIZONTAL, label='Rojo (R)', state='disabled', troughcolor=disabled_color)
-bar_R.grid(column=0, row=9)
-bar_G = tk.Scale(root, from_=0, to=255, length=255, orient=tk.HORIZONTAL, label='Verde (G)', state='disabled', troughcolor=disabled_color)
-bar_G.grid(column=0, row=10)
-bar_B = tk.Scale(root, from_=0, to=255, length=255, orient=tk.HORIZONTAL, label='Azul (B)', state='disabled', troughcolor=disabled_color)
-bar_B.grid(column=0, row=11)
+alien_option = tk.Radiobutton(options_canvas, text='Alien', width=25,value=3, variable=selected_effect)
+options_canvas.create_window(-30, 210, anchor=tk.NW, window=alien_option)
+bar_R = tk.Scale(options_canvas, from_=0, to=255, length=255, orient=tk.HORIZONTAL, label='Rojo (R)', state='disabled', troughcolor=disabled_color)
+options_canvas.create_window(64, 240, anchor=tk.NW, window=bar_R)
+bar_G = tk.Scale(options_canvas, from_=0, to=255, length=255, orient=tk.HORIZONTAL, label='Verde (G)', state='disabled', troughcolor=disabled_color)
+options_canvas.create_window(64, 300, anchor=tk.NW, window=bar_G)
+bar_B = tk.Scale(options_canvas, from_=0, to=255, length=255, orient=tk.HORIZONTAL, label='Azul (B)', state='disabled', troughcolor=disabled_color)
+options_canvas.create_window(64, 360, anchor=tk.NW, window=bar_B)
 # -- Poster
-poster_option = tk.Radiobutton(root, text='Poster', width=25,value=4, variable=selected_effect)
-poster_option.grid(column=0,row=12)
-bar_clusters = tk.Scale(root, from_=0, to=150, length=255, orient=tk.HORIZONTAL, label='Clusters (k)', state='disabled', troughcolor=disabled_color)
-bar_clusters.grid(column=0,row=13)
+more_precission = tk.BooleanVar()
+poster_option = tk.Radiobutton(options_canvas, text='Poster', width=25,value=4, variable=selected_effect)
+options_canvas.create_window(-27, 430, anchor=tk.NW, window=poster_option)
+bar_clusters = tk.Scale(options_canvas, from_=0, to=150, length=255, orient=tk.HORIZONTAL, label='Clusters (k)', state='disabled', troughcolor=disabled_color)
+options_canvas.create_window(64, 460, anchor=tk.NW, window=bar_clusters)
+checkbox_more_precission = tk.Checkbutton(root, text="Más precisión", variable=more_precission, state='disabled')
+options_canvas.create_window(62, 530, anchor=tk.NW, window=checkbox_more_precission)
+
 # -- Distorsion
-distorsion_option = tk.Radiobutton(root, text='Distorsion', width=25,value=5, variable=selected_effect)
-distorsion_option.grid(column=0,row=14)
-bar_amount = tk.Scale(root, variable=tk.IntVar(value=0), from_=-1, to=1, length=255, resolution=0.05, orient=tk.HORIZONTAL, label='Cantidad', state='disabled', troughcolor=disabled_color)
-bar_amount.grid(column=0,row=15)
+distorsion_option = tk.Radiobutton(options_canvas, text='Distorsion', width=25,value=5, variable=selected_effect)
+options_canvas.create_window(-20, 570, anchor=tk.NW, window=distorsion_option)
+bar_coefficient = tk.Scale(options_canvas, variable=tk.DoubleVar(value=0), from_=-10e-6, to=10e-6, length=255, resolution=10e-7, orient=tk.HORIZONTAL, label='Primer coeficiente', state='disabled', troughcolor=disabled_color)
+options_canvas.create_window(64, 600, anchor=tk.NW, window=bar_coefficient)
+bar_x_center_pos = tk.Scale(options_canvas, variable=tk.IntVar(value=cam_width/2), from_=0, to=cam_width, length=255, resolution=1, orient=tk.HORIZONTAL, label='Posicion del centro en X', state='disabled', troughcolor=disabled_color)
+options_canvas.create_window(64, 660, anchor=tk.NW, window=bar_x_center_pos)
+bar_y_center_pos = tk.Scale(options_canvas, variable=tk.IntVar(value=cam_height/2), from_=0, to=cam_height, length=255, resolution=1, orient=tk.HORIZONTAL, label='Posicion del centro en Y', state='disabled', troughcolor=disabled_color)
+options_canvas.create_window(64, 720, anchor=tk.NW, window=bar_y_center_pos)
 
-# -- Meter GUI en la ventana.
+def reset_values():
+    selected_effect.set(0)
+    bar_contrast.set(1.0)
+    bar_brightness.set(0)
+    bar_R.set(0)
+    bar_G.set(0)
+    bar_B.set(0)
+    bar_clusters.set(0)
+    more_precission.set(False)
+    bar_coefficient.set(0)
+    bar_x_center_pos.set(cam_width/2)
+    bar_y_center_pos.set(cam_height/2)
 
-# WEBCAM CAPTURE
-cam = cv.VideoCapture(0)
-last_frame = None
+button_reset = tk.Button(options_canvas, text="Reiniciar", command=reset_values)
+options_canvas.create_window(265, 20, anchor=tk.NW, window=button_reset)
+
 
 def disable_option(n):
     if (n == 1):
@@ -190,9 +227,12 @@ def disable_option(n):
         bar_G.config(state='disabled', troughcolor=disabled_color)
         bar_B.config(state='disabled', troughcolor=disabled_color)
     elif (n == 4):
-        bar_clusters(state='disabled', troughcolor=disabled_color)
+        bar_clusters.config(state='disabled', troughcolor=disabled_color)
+        checkbox_more_precission.config(state='disabled')
     elif (n == 5):
-        bar_amount(state='disabled', troughtcolor=disabled_color)
+        bar_coefficient.config(state='disabled', troughcolor=disabled_color)
+        bar_x_center_pos.config(state='disabled', troughcolor=disabled_color)
+        bar_y_center_pos.config(state='disabled', troughcolor=disabled_color)
 
 def effects(frame):
     global previous_effect
@@ -207,24 +247,7 @@ def effects(frame):
         bar_brightness.config(state='normal', troughcolor=enabled_color)
         frame = contrast_and_brightness(frame, a, b)
     elif (n == 2):
-        frame = cv.cvtColor(frame, cv.COLOR_BGR2YCrCb)
-        y, cr, cb = cv.split(frame)
-        # https://docs.opencv.org/4.x/d1/db7/tutorial_py_histogram_begins.html
-        # Images: [frame].
-        # Channels: [0] if grayscale, [1] if blue, [2] if green, [3] if red.
-        # Mask: mask image.
-        # HistSize: BIN count.
-        # Ranges: histogram range.
-        hist = cv.calcHist([y], [0], None, [256], [0, 256])
-        cdf  = hist.cumsum()
-        # Para normalizar:
-        # - Multiplicamos el cumsum por el valor de escala de grises más alto.
-        # - Dividimos el cumsum por el número total de pixeles (que equivale al máximo del cumsum).
-        cdf_m = np.ma.masked_equal(cdf, 0)
-        cdf_m = (cdf_m - cdf_m.min())*255/(cdf_m.max()-cdf_m.min())
-        cdf = np.ma.filled(cdf_m, 0).astype('uint8')
-        y = cdf[y]
-        frame = cv.cvtColor(cv.merge((y, cr, cb)), cv.COLOR_YCR_CB2BGR)
+        frame = histogram_equalization(frame)
     elif (n == 3):
         color = (
             bar_B.get(),
@@ -237,19 +260,22 @@ def effects(frame):
         frame = alien_skin(frame, color)
     elif (n == 4):
         bar_clusters.config(state='normal', troughcolor=enabled_color)
-        frame = poster(frame, bar_clusters.get())
+        checkbox_more_precission.config(state='normal')
+        frame = poster(frame, bar_clusters.get(), more_precission.get())
     elif (n == 5):
-        print('Distorsion')
- 
+        bar_coefficient.config(state='normal', troughcolor=enabled_color)
+        bar_x_center_pos.config(state='normal', troughcolor=enabled_color)
+        bar_y_center_pos.config(state='normal', troughcolor=enabled_color)
+        frame = barrel_pincussion(frame, bar_coefficient.get(), bar_x_center_pos.get(), bar_y_center_pos.get())
     previous_effect = n
     return frame
 
 def update_view():
     check, frame = cam.read()
     if (check):
-        frame       = cv.flip(frame, 1)
+        frame       = cv.flip(cv.resize(frame, (int(frame.shape[1] * 0.85), int(frame.shape[0] * 0.85)), interpolation=cv.INTER_AREA), 1)
         # Source image:
-        source      = cv.resize(cv.cvtColor(frame, cv.COLOR_BGR2RGB,1), (int(frame.shape[1] * 0.5), int(frame.shape[0] * 0.5)), interpolation=cv.INTER_AREA)
+        source      = cv.cvtColor(frame, cv.COLOR_BGR2RGB,1)
         img_frame   = Image.fromarray(source)
         imgtk_frame = ImageTk.PhotoImage(image=img_frame)
         source_image.imgtk = imgtk_frame
