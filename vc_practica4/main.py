3ed03e73a0c2229b8b99a9e5ab321a7aa5ec8c1e
d21927790d <dedepankeke@gmail.com>
2023-04-27 13:08:25 +0200

    Practica 4 (semi)

--- vc_practica4/main.py
+++ vc_practica4/main.py
@@ -23,19 +23,42 @@ def SIFT(img):
 def ORB(img):
     grayscale = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     orb       = cv.ORB_create()
-    kp        = orb.detect(grayscale, None)
-    return cv.drawKeypoints(img, kp, None, color=(0,255,0), flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    kp, des   = orb.detectAndCompute(grayscale, None)
+    return kp, des, cv.drawKeypoints(img, kp, None, color=(0,255,0), flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
 # https://docs.opencv.org/4.x/db/d70/tutorial_akaze_matching.html
 def AKAZE(img):
     grayscale = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     akaze     = cv.AKAZE_create()
-    kp        = akaze.detect(grayscale, None)
+    kp, des   = akaze.detect(grayscale, None)
     return cv.drawKeypoints(img, kp, None, color=(0,255,0), flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
-img = cv.imread('./BuildingScene/building1.JPG')
+# Cambiar lo que devuelven los operadores:
+def BRUTE_MATCH(fst, snd, operator):
+    if (operator == 'Harris'):
+        HARRIS(fst)
+        HARRIS(snd)
+        return []
+    elif (operator == 'Sift'):
+        SIFT(fst)
+        SIFT(snd)
+        return []
+    elif (operator == 'Orb'):
+        fst_kp, fst_des, _ = ORB(fst)
+        snd_kp, snd_des, _ = ORB(snd)
+        bf      = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=True)
+        matches = bf.match(fst_des, snd_des)
+        matches = sorted(matches, key = lambda x:x.distance)
+        return cv.drawMatches(fst, fst_kp, snd, snd_kp, matches[:10], None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+    elif (operator == 'Akaze'):
+        AKAZE(fst)
+        AKAZE(snd)
+        return []
+
+fst = cv.imread('./BuildingScene/building1.JPG')
+snd = cv.imread('./BuildingScene/building2.JPG')
 cv.namedWindow('Practica 4', cv.WINDOW_AUTOSIZE)
-cv.imshow('Practica 4', AKAZE(img))
+cv.imshow('Practica 4', BRUTE_MATCH(fst, snd, 'Orb'))
 cv.waitKey(0)
 
 # while 1:
