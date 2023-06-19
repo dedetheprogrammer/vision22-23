add5472234cca1cd99f311f907ac1dc1d7908d0c
d21927790d <dedepankeke@gmail.com>
2023-06-19 12:26:07 +0200

    PR1: Efecto pixelización + bordes de neón

--- vc_practica1/practica1_735976_780131.py
+++ vc_practica1/practica1_735976_780131.py
@@ -8,6 +8,9 @@ import tkinter as tk
 def rgb_to_hex(r, g, b):
     return '#{:02x}{:02x}{:02x}'.format(r, g, b)
 
+def clip(frame):
+    return np.clip(frame, 0, 255).astype(np.uint8)
+
 # =============================================================================
 # Efectos
 # =============================================================================
@@ -126,6 +129,23 @@ def barrel_pincussion(img, k1, center_x, center_y): #k2, center_x, center_y):
     yd   = y + ((y - center_y) * k1 * r2) #+ ((y - center_y) * k2 * r4)
     return cv.remap(img, xd.astype(np.float32), yd.astype(np.float32), cv.INTER_LINEAR)
 
+# Pixelize
+def pixelize(img, size):
+    if (size == 0):
+        return img
+
+    height, width, _ = img.shape
+    p_img = cv.resize(img, (width // size, height // size), interpolation=cv.INTER_NEAREST)
+    return cv.resize(p_img, (width, height), interpolation=cv.INTER_NEAREST)
+
+# Neon borders
+def neon_borders(frame, sigma, threshold):
+    sframe = cv.cvtColor(clip(cv.GaussianBlur(src=frame, ksize=(5,5), sigmaX=sigma)), cv.COLOR_BGR2GRAY)
+    gX     = cv.Sobel(sframe, cv.CV_64F, 1, 0, ksize=3)
+    gY     = cv.Sobel(sframe, cv.CV_64F, 0, 1, ksize=3) * -1
+    _, mod = cv.threshold(np.sqrt(gX**2 + gY**2).astype(np.uint8), threshold, 255, cv.THRESH_BINARY)
+    return cv.bitwise_and(frame, frame, mask=mod)
+
 # =============================================================================
 # Main
 # =============================================================================
@@ -141,6 +161,8 @@ cam_height = int(cam.get(cv.CAP_PROP_FRAME_HEIGHT) * 0.85)
 # - The window:
 root = tk.Tk()
 root.title('Practica 1')
+root.geometry(f'1300x{root.winfo_screenheight()}')
+root.resizable(width=False, height=False)
 root.rowconfigure(0, minsize=20)
 root.rowconfigure(3, minsize=20)
 root.columnconfigure(0, minsize=-10)
@@ -172,44 +194,59 @@ previous_effect = 0
 selected_effect = tk.IntVar()
 # -- Ningun efecto
 none_option     = tk.Radiobutton(options_canvas, text='Ninguno', width=25,value=0, variable=selected_effect)
-options_canvas.create_window(-20, 20, anchor=tk.NW, window=none_option)
+options_canvas.create_window(-50, 20, anchor=tk.NW, window=none_option)
 # -- Contraste
 contrast_option = tk.Radiobutton(options_canvas, text='Contraste y brillo', width=25,value=1, variable=selected_effect)
-options_canvas.create_window(1, 50, anchor=tk.NW, window=contrast_option)
-bar_contrast    = tk.Scale(options_canvas, variable=tk.DoubleVar(value=1.0), from_=-3, to=3, resolution=0.1, length=255, orient=tk.HORIZONTAL,state='disabled', troughcolor=disabled_color)
-options_canvas.create_window(64, 70, anchor=tk.NW, window=bar_contrast)
-bar_brightness  = tk.Scale(options_canvas, variable=tk.IntVar(value=0), from_=-512, to=512, length=255, orient=tk.HORIZONTAL, state='disabled', troughcolor=disabled_color)
-options_canvas.create_window(64, 125, anchor=tk.NW, window=bar_brightness)
-# Ecualizacion de histograma
+options_canvas.create_window(-29, 50, anchor=tk.NW, window=contrast_option)
+bar_contrast    = tk.Scale(options_canvas, variable=tk.DoubleVar(value=1.0), from_=-3, to=3, resolution=0.1, length=245, orient=tk.HORIZONTAL,state='disabled', troughcolor=disabled_color)
+options_canvas.create_window(34, 70, anchor=tk.NW, window=bar_contrast)
+bar_brightness  = tk.Scale(options_canvas, variable=tk.IntVar(value=0), from_=-512, to=512, length=245, orient=tk.HORIZONTAL, state='disabled', troughcolor=disabled_color)
+options_canvas.create_window(34, 125, anchor=tk.NW, window=bar_brightness)
+
+# -- Pixelize
+pixelize_option = tk.Radiobutton(options_canvas, text='Pixelizar', width=25, value=6, variable=selected_effect)
+options_canvas.create_window(240, 50, anchor=tk.NW, window=pixelize_option)
+bar_pixelize    = tk.Scale(options_canvas, variable=tk.IntVar(value=0.0), from_=0, to=50, label='Factor de pixelización', length=245, orient=tk.HORIZONTAL, state='disabled', troughcolor=disabled_color)
+options_canvas.create_window(320, 85, anchor=tk.NW, window=bar_pixelize)
+
+# -- Neon borders
+neon_option = tk.Radiobutton(options_canvas, text='Bordes de neon', width=25,value=7, variable=selected_effect)
+options_canvas.create_window(260, 180, anchor=tk.NW, window=neon_option)
+bar_neon_sigma = tk.Scale(options_canvas, variable=tk.IntVar(value=0), from_=0, to=10, length=245, orient=tk.HORIZONTAL,state='disabled', troughcolor=disabled_color, label='Sigma')
+options_canvas.create_window(320, 210, anchor=tk.NW, window=bar_neon_sigma)
+bar_neon_threshold = tk.Scale(options_canvas, variable=tk.IntVar(value=0), from_=0, to=255, length=245, orient=tk.HORIZONTAL,state='disabled', troughcolor=disabled_color, label='Threshold')
+options_canvas.create_window(320, 275, anchor=tk.NW, window=bar_neon_threshold)
+
+#-- Ecualizacion de histograma
 equalize_option = tk.Radiobutton(options_canvas, text='Ecualizacion de histograma',width=25, value=2, variable=selected_effect)
-options_canvas.create_window(28, 180, anchor=tk.NW, window=equalize_option)
+options_canvas.create_window(-2, 180, anchor=tk.NW, window=equalize_option)
 # -- Alien
 alien_option = tk.Radiobutton(options_canvas, text='Alien', width=25,value=3, variable=selected_effect)
-options_canvas.create_window(-30, 210, anchor=tk.NW, window=alien_option)
+options_canvas.create_window(-60, 210, anchor=tk.NW, window=alien_option)
 bar_R = tk.Scale(options_canvas, from_=0, to=255, length=255, orient=tk.HORIZONTAL, label='Rojo (R)', state='disabled', troughcolor=disabled_color)
-options_canvas.create_window(64, 240, anchor=tk.NW, window=bar_R)
+options_canvas.create_window(34, 240, anchor=tk.NW, window=bar_R)
 bar_G = tk.Scale(options_canvas, from_=0, to=255, length=255, orient=tk.HORIZONTAL, label='Verde (G)', state='disabled', troughcolor=disabled_color)
-options_canvas.create_window(64, 300, anchor=tk.NW, window=bar_G)
+options_canvas.create_window(34, 300, anchor=tk.NW, window=bar_G)
 bar_B = tk.Scale(options_canvas, from_=0, to=255, length=255, orient=tk.HORIZONTAL, label='Azul (B)', state='disabled', troughcolor=disabled_color)
-options_canvas.create_window(64, 360, anchor=tk.NW, window=bar_B)
+options_canvas.create_window(34, 360, anchor=tk.NW, window=bar_B)
 # -- Poster
 more_precission = tk.BooleanVar()
 poster_option = tk.Radiobutton(options_canvas, text='Poster', width=25,value=4, variable=selected_effect)
-options_canvas.create_window(-27, 430, anchor=tk.NW, window=poster_option)
+options_canvas.create_window(-57, 430, anchor=tk.NW, window=poster_option)
 bar_clusters = tk.Scale(options_canvas, from_=0, to=150, length=255, orient=tk.HORIZONTAL, label='Clusters (k)', state='disabled', troughcolor=disabled_color)
-options_canvas.create_window(64, 460, anchor=tk.NW, window=bar_clusters)
+options_canvas.create_window(34, 460, anchor=tk.NW, window=bar_clusters)
 checkbox_more_precission = tk.Checkbutton(root, text="Más precisión", variable=more_precission, state='disabled')
-options_canvas.create_window(62, 530, anchor=tk.NW, window=checkbox_more_precission)
+options_canvas.create_window(32, 530, anchor=tk.NW, window=checkbox_more_precission)
 
 # -- Distorsion
 distorsion_option = tk.Radiobutton(options_canvas, text='Distorsion', width=25,value=5, variable=selected_effect)
-options_canvas.create_window(-20, 570, anchor=tk.NW, window=distorsion_option)
+options_canvas.create_window(-50, 570, anchor=tk.NW, window=distorsion_option)
 bar_coefficient = tk.Scale(options_canvas, variable=tk.DoubleVar(value=0), from_=-10e-6, to=10e-6, length=255, resolution=10e-7, orient=tk.HORIZONTAL, label='Primer coeficiente', state='disabled', troughcolor=disabled_color)
-options_canvas.create_window(64, 600, anchor=tk.NW, window=bar_coefficient)
+options_canvas.create_window(34, 600, anchor=tk.NW, window=bar_coefficient)
 bar_x_center_pos = tk.Scale(options_canvas, variable=tk.IntVar(value=cam_width/2), from_=0, to=cam_width, length=255, resolution=1, orient=tk.HORIZONTAL, label='Posicion del centro en X', state='disabled', troughcolor=disabled_color)
-options_canvas.create_window(64, 660, anchor=tk.NW, window=bar_x_center_pos)
+options_canvas.create_window(34, 660, anchor=tk.NW, window=bar_x_center_pos)
 bar_y_center_pos = tk.Scale(options_canvas, variable=tk.IntVar(value=cam_height/2), from_=0, to=cam_height, length=255, resolution=1, orient=tk.HORIZONTAL, label='Posicion del centro en Y', state='disabled', troughcolor=disabled_color)
-options_canvas.create_window(64, 720, anchor=tk.NW, window=bar_y_center_pos)
+options_canvas.create_window(34, 720, anchor=tk.NW, window=bar_y_center_pos)
 
 def reset_values():
     selected_effect.set(0)
@@ -223,6 +260,9 @@ def reset_values():
     bar_coefficient.set(0)
     bar_x_center_pos.set(cam_width/2)
     bar_y_center_pos.set(cam_height/2)
+    bar_pixelize.set(0)
+    bar_neon_sigma.set(0)
+    bar_neon_threshold.set(0)
 
 button_reset = tk.Button(options_canvas, text="Reiniciar", command=reset_values)
 options_canvas.create_window(265, 20, anchor=tk.NW, window=button_reset)
@@ -242,7 +282,12 @@ def disable_option(n):
         bar_coefficient.config(state='disabled', troughcolor=disabled_color)
         bar_x_center_pos.config(state='disabled', troughcolor=disabled_color)
         bar_y_center_pos.config(state='disabled', troughcolor=disabled_color)
-
+    elif (n == 6):
+        bar_pixelize.config(state='disabled', troughcolor=disabled_color)
+    elif (n == 7):
+        bar_neon_sigma.config(state='disabled', troughcolor=disabled_color)
+        bar_neon_threshold.config(state='disabled', troughcolor=disabled_color)
+        
 def effects(frame):
     global previous_effect
     a = bar_contrast.get()
@@ -276,6 +321,14 @@ def effects(frame):
         bar_x_center_pos.config(state='normal', troughcolor=enabled_color)
         bar_y_center_pos.config(state='normal', troughcolor=enabled_color)
         frame = barrel_pincussion(frame, bar_coefficient.get(), bar_x_center_pos.get(), bar_y_center_pos.get())
+    elif (n == 6):
+        bar_pixelize.config(state='normal', troughcolor=enabled_color)
+        frame = pixelize(frame, bar_pixelize.get())
+    elif (n == 7):
+        bar_neon_sigma.config(state='normal', troughcolor=enabled_color)
+        bar_neon_threshold.config(state='normal', troughcolor=enabled_color)
+        frame = neon_borders(frame, bar_neon_sigma.get(), bar_neon_threshold.get())
+        
     previous_effect = n
     return frame
 
